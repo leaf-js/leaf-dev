4f6a4b0b7b71195da7f3a2384493749c3e396a5c
MinnShinSawLingMyat <lma@minshinsaw.com>
2024-09-19 10:33:21 +0630

    first commit

---
+++ leaf.js
@@ -0,0 +1,176 @@
+window.Leaf = {
+    Queue : class {
+
+        queue = [];
+
+        /**
+         * Adds a new element to the end of the queue.
+         *
+         * @param {*} element - the element to be added to the queue
+         * @return {undefined} this function does not return any value
+         */
+        enqueue(element) {
+            this.queue.push(element);
+        }
+
+        /**
+         * Removes and returns the front element from the queue.
+         *
+         * @return {*} The front element from the queue, or undefined if the queue is empty.
+         */
+        dequeue() {
+            return this.queue.shift();
+        }
+
+        /**
+         * Returns the front element from the queue without removing it.
+         *
+         * @return {*} The front element from the queue, or undefined if the queue is empty.
+         */
+        peek() {
+            return $this.queue[0];
+        }
+
+        /**
+         * Checks if the queue is empty.
+         *
+         * @return {boolean} True if the queue is empty, false otherwise.
+         */
+        isEmpty() {
+            return this.queue.length === 0;
+        }
+
+        /**
+         * Returns the number of elements in the queue.
+         *
+         * @return {number} The size of the queue.
+         */
+        size() {
+            return this.queue.length;
+        }
+
+    },
+
+    directives : {
+
+        'l-text': (el, value) => {
+
+            el.innerText = value
+
+        },
+
+        'l-show': (el, value) => {
+
+            el.style.display = value ? 'block' : 'none'
+
+        }
+
+    },
+
+    init(data) {
+
+        this.root = document.querySelector('#leaf-root')
+        this.data = this.observe(data)
+
+        this.registerListeners()
+        this.refreshDom()
+
+    },
+
+    observe(data) {
+
+        var self = this
+
+        return new Proxy(data, {
+
+            set(target, key, value) {
+
+                target[key] = value
+
+
+                self.refreshDom()
+
+            }
+
+        })
+    },
+
+    registerListeners() {
+
+        this.walkDomBFS(this.root, el => {
+
+            Array.from(el.attributes).forEach(attribute => {
+
+                if (! attribute.name.startsWith('@')) return
+
+                let event = attribute.name.replace('@', '')
+                
+                el.addEventListener(event, () => {
+
+                    if( this.isArrowFunction(attribute.value) ) {
+
+                        eval(`with (this.data) { (${attribute.value}) }`)()
+
+                    } else if (this.data[attribute.value]) {
+
+                        eval(`with (this.data) { (${attribute.value}()) }`)
+
+                    } else {
+
+                        eval(`with (this.data) { (${attribute.value}) }`)
+                        
+                    }
+
+                })
+
+            })
+
+        })
+    },
+
+    refreshDom() {
+
+        this.walkDomBFS(this.root, el => {
+            
+            Array.from(el.attributes).forEach(attribute => {
+
+                if (! Object.keys(this.directives).includes(attribute.name)) return
+
+                this.directives[attribute.name](el, eval(`with (this.data) { (${attribute.value}) }`))
+
+            })
+            
+        })
+
+    },
+
+    walkDomBFS(el, callback) {
+
+        const queue = new this.Queue();
+
+        queue.enqueue(el);
+
+        while (!queue.isEmpty()) {
+            const currentEl = queue.dequeue();
+
+            if(currentEl) {
+                callback(currentEl);
+
+                for (const child of currentEl.children) {
+                    queue.enqueue(child);
+                }
+            }
+            
+        }
+
+    },
+
+    isArrowFunction (value) {
+
+        const arrowFunctionPattern = /^\(\s*\)\s*=>\s*/;
+        return arrowFunctionPattern.test(value);
+
+    }
+}
+
+
